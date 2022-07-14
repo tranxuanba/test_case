24dae660300b2980326fdfe7e7aa2d29c0f68f4e
ishopjapan <thoitran2107@gmail.com>
2022-07-15 00:04:35 +0700

    test case 3 khong qua

--- testCase/NextDayTest.java
+++ testCase/NextDayTest.java
@@ -33,4 +33,19 @@ class NextDayTest {
         //so sanh
         assertEquals(expect, result);
     }
+
+    @Test
+    @DisplayName("test case 2")
+    void testDay30Month4Year2018() {
+        //chuan bi du lieu
+        int dayTest = 30;
+        int monthTest = 4;
+        int yearTest = 2018;
+
+        String expect = "1/5/2018";
+        //chay phuong thuc
+        String result = NextDay.getNextDay(dayTest, monthTest, yearTest);
+        //so sanh
+        assertEquals(expect, result);
+    }
 }
