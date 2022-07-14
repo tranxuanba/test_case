fe734db4cee3834406309268c275800bdb2fd041
ishopjapan <thoitran2107@gmail.com>
2022-07-14 23:56:17 +0700

    qua test 2

--- src/NextDay.java
+++ src/NextDay.java
@@ -4,8 +4,14 @@ public class NextDay {
     public static final String CONCATERNATION = "/";
 
     public static String getNextDay(int dayTest, int monthTest, int yearTest) {
-        int day = dayTest + 1;
-        String result = day+ CONCATERNATION +monthTest+CONCATERNATION+yearTest;
+        int day;
+        if (dayTest == 31) {
+            day = 1;
+            monthTest = monthTest + 1;
+        } else {
+            day = dayTest + 1;
+        }
+        String result = day + CONCATERNATION + monthTest + CONCATERNATION + yearTest;
         return result;
     }
 }
